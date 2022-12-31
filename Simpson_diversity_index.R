3b539c78f383b3939ff22c5375657cb869bdf9ac
IsaiasML10 <112969083+IsaiasML10@users.noreply.github.com>
2022-12-31 11:50:12 -0500

    Update Simpson_diversity_index.R

--- Simpson_diversity_index.R
+++ Simpson_diversity_index.R
@@ -5,5 +5,5 @@ simpson
 hist(simpson)
 
 #MLST debe ser reemplazado con el nombre del archivo input
-#hist(simpson) se utiliza en caso de querer hacer un histograma del índice
-#de simpson
+#hist(simpson) se utiliza en caso de querer hacer un histograma del índice de simpson
+#Se puede determinar otros índices como el de Shannon, solo reemplazando el nombre de simpson
