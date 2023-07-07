ec34cc20559d29809927449b1ed5d83751f237f7
IsaiasML10 <112969083+IsaiasML10@users.noreply.github.com>
2023-07-06 20:36:30 -0500

    Add files via upload
    file:///home/isaias/Desktop/R/Simprof%2010X.png

---
+++ simprof: Similiarity Profile Analysis.R
@@ -0,0 +1,16 @@
+install.packages("devtools", dependencies = TRUE)
+library(devtools)
+install_github("cran/clustsig") 
+library(clustsig)
+Kp_AMR_VF_150922 <- read.csv("~/Desktop/R/pca_v3_corte_10Q.csv", row.names=1)
+
+Kpsimprof <- simprof(Kp_AMR_VF_150922, num.expected=1000, num.simulated=999,
+                     method.cluster="average", method.distance="euclidean",
+                     method.transform="identity", alpha=0.05,
+                     sample.orientation="row", const=0,
+                     silent=TRUE, increment=100,
+                     undef.zero=TRUE, warn.braycurtis=TRUE)
+
+simprof.plot(Kpsimprof, leafcolors=NA, plot=TRUE, fill=TRUE,
+             leaflab="none", siglinetype=1)
+
